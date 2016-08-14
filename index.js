a9f714e423922cf06c4fdcacfaecaed625472934
coderLiH <284304055@qq.com>
2016-08-14 15:06:42 +0800

    hover效果

---
+++ index.js
@@ -0,0 +1,141 @@
+$(document).ready(function(){
+	$("#YGUMMI").mouseenter(function(){
+		$("#YGUMMI_hover").css("opacity","1.0");
+	});
+	$("#YGUMMI").mouseleave(function(){
+		$("#YGUMMI_hover").css("opacity","0.0");
+	});
+
+	$("#YGFAM").mouseenter(function(){
+		$("#YGFAM_hover").css("opacity","1.0");
+	});
+	$("#YGFAM").mouseleave(function(){
+		$("#YGFAM_hover").css("opacity","0.0");
+	});
+
+	$("#GRIS").mouseenter(function(){
+		$("#GRIS_hover").css("opacity","1.0");
+	});
+	$("#GRIS").mouseleave(function(){
+		$("#GRIS_hover").css("opacity","0.0");
+	});
+
+	$("#MF").mouseenter(function(){
+		$("#MF_hover").css("opacity","1.0");
+	});
+	$("#MF").mouseleave(function(){
+		$("#MF_hover").css("opacity","0.0");
+	});
+
+	$("#BZ").mouseenter(function(){
+		$("#BZ_hover").css("opacity","1.0");
+	});
+	$("#BZ").mouseleave(function(){
+		$("#BZ_hover").css("opacity","0.0");
+	});
+
+	$("#GD").mouseenter(function(){
+		$("#GD_hover").css("opacity","1.0");
+		$("#GD").css("border-width","0.0");
+		$("#GD").css("width","60px");
+		$("#GD").css("height","60px");
+	});
+	$("#GD").mouseleave(function(){
+		$("#GD_hover").css("opacity","0.0");
+		$("#GD").css("border-width","3.0");
+		$("#GD").css("width","54px");
+		$("#GD").css("height","54px");
+	});
+
+	$("#Share").mouseenter(function(){
+		$("#Share_hover").css("opacity","1.0");
+		$("#Share").css("border-width","0.0");
+		$("#Share").css("width","60px");
+		$("#Share").css("height","60px");
+	});
+	$("#Share").mouseleave(function(){
+		$("#Share_hover").css("opacity","0.0");
+		$("#Share").css("border-width","3.0");
+		$("#Share").css("width","54px");
+		$("#Share").css("height","54px");
+	});
+
+	$("#Shenpi").mouseenter(function(){
+		$("#Shenpi_hover").css("opacity","1.0");
+		$("#Shenpi").css("border-width","0.0");
+		$("#Shenpi").css("width","125px");
+		$("#Shenpi").css("height","60px");
+	});
+	$("#Shenpi").mouseleave(function(){
+		$("#Shenpi_hover").css("opacity","0.0");
+		$("#Shenpi").css("border-width","3.0");
+		$("#Shenpi").css("width","119px");
+		$("#Shenpi").css("height","54px");
+	});
+	
+	$("#Update").mouseenter(function(){
+		$("#Update_hover").css("opacity","1.0");
+		$("#Update").css("border-width","0.0");
+		$("#Update").css("width","125px");
+		$("#Update").css("height","125px");
+	});
+	$("#Update").mouseleave(function(){
+		$("#Update_hover").css("opacity","0.0");
+		$("#Update").css("border-width","3.0");
+		$("#Update").css("width","119px");
+		$("#Update").css("height","119px");
+	});
+	
+	$("#Shoudian").mouseenter(function(){
+		$("#Shoudian_hover").css("opacity","1.0");
+	});
+	$("#Shoudian").mouseleave(function(){
+		$("#Shoudian_hover").css("opacity","0.0");
+	});
+
+	$("#Qr").mouseenter(function(){
+		$("#Qr_hover").css("opacity","1.0");
+	});
+	$("#Qr").mouseleave(function(){
+		$("#Qr_hover").css("opacity","0.0");
+	});
+
+	$("#Jiesuan").mouseenter(function(){
+		$("#Jiesuan_hover").css("opacity","1.0");
+		$("#Jiesuan").css("border-width","0.0");
+		$("#Jiesuan").css("width","125px");
+		$("#Jiesuan").css("height","125px");
+	});
+	$("#Jiesuan").mouseleave(function(){
+		$("#Jiesuan_hover").css("opacity","0.0");
+		$("#Jiesuan").css("border-width","3.0");
+		$("#Jiesuan").css("width","119px");
+		$("#Jiesuan").css("height","119px");
+	});
+
+	$("#Yuanze").mouseenter(function(){
+		$("#Yuanze_hover").css("opacity","1.0");
+		$("#Yuanze").css("border-width","0.0");
+		$("#Yuanze").css("width","60px");
+		$("#Yuanze").css("height","60px");
+	});
+	$("#Yuanze").mouseleave(function(){
+		$("#Yuanze_hover").css("opacity","0.0");
+		$("#Yuanze").css("border-width","3.0");
+		$("#Yuanze").css("width","54px");
+		$("#Yuanze").css("height","54px");
+	});
+	
+	$("#Kecheng").mouseenter(function(){
+		$("#Kecheng_hover").css("opacity","1.0");
+		$("#Kecheng").css("border-width","0.0");
+		$("#Kecheng").css("width","60px");
+		$("#Kecheng").css("height","60px");
+	});
+	$("#Kecheng").mouseleave(function(){
+		$("#Kecheng_hover").css("opacity","0.0");
+		$("#Kecheng").css("border-width","3.0");
+		$("#Kecheng").css("width","54px");
+		$("#Kecheng").css("height","54px");
+	});
+});
